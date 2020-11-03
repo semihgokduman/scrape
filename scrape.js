dd99792188bd305c7944cfd3c819dfd4d65e76c3
Semih Gökduman <semihgokduman@gmail.com>
2020-11-03 23:24:17 +0100

    init

---
+++ scrape.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+axios('https://lobste.rs/')
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const title = $(".cur_url").text();
+    console.log(title);
+  })
+  .catch(console.error);
+
+axios("https://lobste.rs/")
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const storyItem = $(".story");
+    const stories = [];
+    storyItem.each(function () {
+      const title = $(this).find(".u-url").text();
+      const domain = $(this).find(".domain").text();
+      const points = $(this).find(".score").text();
+      stories.push({
+        title,
+        domain,
+        points,
+      });
+    });
+    console.log(stories);
+  })
+  .catch(console.error);
